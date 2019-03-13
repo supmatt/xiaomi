c168bcfd6ca35b5200e636539bd52fe5ab85d302
Supmatt <30627680+supmatt@users.noreply.github.com>
2019-03-13 15:49:20 -0400

    repair submit button problem

--- ls-blog-code-mi/back.js
+++ ls-blog-code-mi/back.js
@@ -18,7 +18,7 @@ var app = {
 			//购物车页面
 			this.cartFn();
 			//提交订单页面。
-			this.orderFn();
+//			this.orderFn();
 		} catch (e) {
 			// 还没加载出来
 		}
@@ -51,14 +51,14 @@ var app = {
 			//点击加入购物车
 			$("#J_buyBtnBox a")[0] && $("#J_buyBtnBox a")[0].click();
 		}
-
+		
 
 	},
 	//跳转到购物车页面
 	goCart() {
 
 		if ($(".J_actBox a:contains('去购物车结算')")[0]) {
-			$(".J_actBox a:contains('去购物车结算')")[0].click();
+//			$(".J_actBox a:contains('去购物车结算')")[0].click();
 			//观察到卡了一s在这里，所以加上这一句，上面那句其实就没有用了。
 			location.href = 'https://static.mi.com/cart/';
 			//跑一个错误，中断线程。不再生成定时器。
@@ -71,32 +71,104 @@ var app = {
 		//阻止弹窗,避免阻断流程
 		window.alert = function(){}
 		//购物车中有大于1件商品，删除
-		//$("#J_cartListBody .item-box:gt(0)").find(".J_delGoods").each(function(index, el) {
-		//	el.click();
-			//确认删除按钮
-		//	$("#J_alertOk")[0] && $("#J_alertOk")[0].click();
-
-		//});
-		//确认删除按钮
-		//$("#J_alertOk")[0] && $("#J_alertOk")[0].click();
-		var interval = 0;
+//		$("#J_cartListBody .item-box:gt(0)").find(".J_delGoods").each(function(index, el) {
+//			el.click();
+//			//确认删除按钮
+//			$("#J_alertOk")[0] && $("#J_alertOk")[0].click();
+//
+//		});
+//		//确认删除按钮
+//		$("#J_alertOk")[0] && $("#J_alertOk")[0].click();
+//		var interval = 200;
 		//将每一条的数量减少到1。
-		//$(".J_minus").each(function(index, el) {
+		$(".J_minus").each(function(index, el) {
 			//数量不为1就减少。
 			//不能用这个while。因为input中的值是异步减少的。
-			//if ($(el).next('input').val() != 1) {
-				interval = 600;
-				el.click();
-			//	el.click();
-			//	el.click();
-			//	el.click();
+//			if ($(el).next('input').val() != 1) {
+//				interval = 200;
+//			setTimeout($(".J_minus").click(),1000);
+//			setTimeout($(".J_minus").click(),1000);
+			
+//			setTimeout(el.click(),1000);
+//			setTimeout(el.click(),1000);
+//			setTimeout(el.click(),1000);
+//			setTimeout(el.click(),1000);
+//			window.onload=function(){
+//            el.click();
+//        }
+//		if ($(".J_minus").next('input').val != 1){
+////			setTimeout( function(){$(".J_minus")[0] && $(".J_minus")[0].click()},1000);
+//			setTimeout($(".J_minus").click(),200);
+//			setTimeout($(".J_minus").click(),200);
+//			setTimeout($(".J_minus").click(),200);
+//			setTimeout($(".J_minus").click(),200);
+//			
+//		$(".J_minus").click()[0];
+//		$(".J_minus").click()[0];
+//		$(".J_minus").click()[0];
+//		$(".J_minus").click()[0];
+////			alert($(this).val());
+//		}else{
+//			console.log("打印内容2")
+//			return;
+//		}else{
+				setTimeout(function() {
+            el.click();
+        }, 200);
+//			setTimeout(function() {
+//            el.click();
+//        }, 200);
+//			setTimeout(function() {
+//            el.click();
+//        }, 200);
+//			setTimeout(function() {
+//            el.click();
+//        }, 200);
+		
+//				el.click();
+//				el.click();
+//				el.click();
+//				el.click();
+//				
+//			}
+//			else{
+//				$("#J_goCheckout")[0] && $("#J_goCheckout")[0].click();
 				
-			//}
-		//});
+				
+//		}
+
+				
+//			}
+//			console.log("打印内容2");
+//			return;
+		
+		}
+	)
+	;
 		//去结算。因为上面的数量减少操作是异步的。所以这里设置一个延时，让上面的操作生效之后再进行这个操作。
-		setTimeout(function() {
-			$("#J_goCheckout")[0] && $("#J_goCheckout")[0].click();
-		}, interval);
+//		setTimeout(function() {
+//            $("#J_goCheckout").click();
+//        }, 3200);
+		//==================================
+					if ($("#J_cartBar a:contains('去结算')")[0]) {
+						setTimeout(function() {
+			$("#J_cartBar a:contains('去结算')")[0].click();
+							}, 7900);
+			//观察到卡了一s在这里，所以加上这一句，上面那句其实就没有用了。
+//			location.href = 'https://static.mi.com/cart/';
+			//跑一个错误，中断线程。不再生成定时器。
+			throw new Error('abcd');	
+			}	
+		//==========================================
+		
+//		setTimeout(function() {
+
+//		setTimeout(function() {
+//            $("#J_goCheckout")[0] && $("#J_goCheckout")[0].click();
+//        }, 6200);
+//		console.log("刷新中");
+
+//		}, interval);
 		//到了购物车页面甚至下订单的页面依然可能会出现售罄的情况。这个时候不要放弃，重新刷新购物车页面继续进行操作。还是有可能刷到的。
 		if ($('h3:contains(抱歉，以下商品已经失效或者暂时售罄):visible').length > 0||$('a:contains(到货提醒):visible').length > 0||$("dt:contains(选中的商品已经全部失效或者暂时售罄):visible").length>0||$("#J_goCheckout").hasClass('btn-disabled')) {
 			//避免请求太过频繁。
@@ -105,6 +177,13 @@ var app = {
 			}, 500)
 
 		}
+		
+		//self innovation
+//		alert($(this).val());
+//		if($("input[name='2183600008_0_buy']") ){
+//			$("input[name='2183600008_0_buy']").val("1");
+//			$("input[name='2183600008_0_buy']").data-num("1");
+//		}
 
 	},
 	//选地址，下单页面
